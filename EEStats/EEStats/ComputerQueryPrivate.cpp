605c24db089cce5778b5322f20a6ebd7b44fd2d3
EnergyCube <dev@energycube.fr>
2022-09-10 14:34:24 +0200

    DllExport fix and new functions (wine/mutex/arch)

--- EEStats/EEStats/ComputerQueryPrivate.cpp
+++ EEStats/EEStats/ComputerQueryPrivate.cpp
@@ -53,8 +53,8 @@ bool ComputerQuery::runInVM()
     return false;
 }
 
-
 /// DLL EXPORT FOR EE COMMUNITY SETUP
+#include <thread>
 #define DllExport extern "C" __declspec(dllexport)
 
 DllExport const char* EEStats_getUID()
@@ -101,11 +101,69 @@ DllExport bool EEStats_runInVM()
 
 DllExport bool EEStats_isWine()
 {
-    return ComputerQuery().isWine();
+    bool result_scope = false;
+
+    auto initLamda = [](void* data) -> unsigned int
+    {
+        bool* lamda_scope = static_cast<bool*>(data);
+        *lamda_scope = ComputerQuery().isWine();
+        return 1;
+    };
+    HANDLE wmiThread = (HANDLE)_beginthreadex(0, 0, initLamda, &result_scope, 0, 0);
+    if (wmiThread != NULL)
+        WaitForSingleObject(wmiThread, INFINITE);
+    return result_scope;
 }
 
 DllExport const char* EEStats_getWineVersion()
 {
-    return ComputerQuery().getWineVersion();
+    char* result_scope = false;
+
+    auto initLamda = [](void* data) -> unsigned int
+    {
+        char** lamda_scope = static_cast<char**>(data);
+        *lamda_scope = (char*)ComputerQuery().getWineVersion();
+        return 1;
+    };
+    HANDLE wmiThread = (HANDLE)_beginthreadex(0, 0, initLamda, &result_scope, 0, 0);
+    if (wmiThread != NULL)
+        WaitForSingleObject(wmiThread, INFINITE);
+    return result_scope;
+}
+
+DllExport const char* EEStats_getProcessorArch()
+{
+    std::string result_scope = "ERROR_UNKNOWN";
+
+    // ComputerQuery use WMI and it require to run in a thread :|
+    auto initLamda = [](void* data) -> unsigned int
+    {
+        std::string* lamda_scope = static_cast<std::string*>(data);
+        if (!lamda_scope)
+            return 0;
+        ComputerQuery gq;
+        *lamda_scope = gq.getProcessorArch();
+        if (lamda_scope->empty())
+            *lamda_scope = "ERROR_GENERATION";
+        return 1;
+    };
+
+    HANDLE wmiThread = (HANDLE)_beginthreadex(0, 0, initLamda, &result_scope, 0, 0);
+    if (wmiThread != NULL)
+        WaitForSingleObject(wmiThread, INFINITE);
+    else
+        result_scope = "ERROR_THREAD";
+    // strdup because std::string will be cleaned, idk how to make that clean without passing a ptr as arg...
+    return _strdup(result_scope.c_str());
+}
+
+DllExport bool EEStats_isEmpireEarthRunning()
+{
+    return OpenMutex(SYNCHRONIZE, TRUE, L"StainlessSteelStudiosPresentsEmpireEarth") != NULL;
+}
+
+DllExport bool EEStats_isTheArtOfConquestRunning()
+{
+    return OpenMutex(SYNCHRONIZE, TRUE, L"MadDocSoftwarePresentsEmpireEarthExpansion") != NULL;
 }
 /// END DLL EXPORT FOR EE COMMUNITY SETUP
